9e787258d9381f541c917f7ef7cf1cb6ddb6817e
nihatkhidirov <nihatmkh@code.edu.az>
2024-06-10 13:23:30 +0400

    Home Page Finished with some missings

---
+++ assets/js/home.js
@@ -0,0 +1,13 @@
+$(".hero-slider").owlCarousel({
+  items: 1,
+  loop: true,
+  nav: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  mouseDrag: false,
+  dots: false,
+  navText: [
+    "<i class='icofont-double-left'></i>",
+    "<i class='icofont-double-right'></i>",
+  ],
+});
